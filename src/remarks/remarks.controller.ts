8c4e68e648df055a74898d058b80be659f807699
iftekhar1405 <iftekharahmedxyz@gmail.com>
2024-11-18 13:36:34 +0530

    remarks findByEnquiryId fixed

--- src/remarks/remarks.controller.ts
+++ src/remarks/remarks.controller.ts
@@ -1,33 +1,43 @@
-import {Body, Controller, Delete, Get, Param, Post, Put, Query} from '@nestjs/common';
-import {RemarksService} from "./remarks.service";
-import {Remark} from "./schema/remarks.schema";
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { RemarksService } from './remarks.service';
+import { Remark } from './schema/remarks.schema';
 
 @Controller('remarks')
 export class RemarksController {
-    constructor(private readonly remarksService: RemarksService) {
-    }
-@Get()
-    async getAll(): Promise<Remark[]> {
-        return this.remarksService.findAll()
-}
-@Get(':id')
-    async getOne(@Param('id') id: string): Promise<Remark> {
-        return this.remarksService.findOne(id)
-}
+    constructor(private readonly remarksService: RemarksService) {}
+
+    // Get all remarks
     @Get()
-    async getRemarksByEnquiryId(@Query('enquiryId') enquiryId: string): Promise<Remark[]> {
-        return this.remarksService.findByEnquiryId(enquiryId);
+    async getAll(@Query('enquiryId') enquiryId: string): Promise<Remark[]> {
+        // If enquiryId is provided, fetch remarks by enquiryId
+        if (enquiryId) {
+            return this.remarksService.findByEnquiryId(enquiryId);
+        }
+        // Otherwise, fetch all remarks
+        return this.remarksService.findAll();
     }
-@Post()
+
+    // Get a specific remark by ID
+    @Get(':id')
+    async getOne(@Param('id') id: string): Promise<Remark> {
+        return this.remarksService.findOne(id);
+    }
+
+    // Create a new remark
+    @Post()
     async create(@Body() data: Partial<Remark>): Promise<Remark> {
-        return this.remarksService.create(data)
-}
-@Put(':id')
+        return this.remarksService.create(data);
+    }
+
+    // Update an existing remark
+    @Put(':id')
     async update(@Param('id') id: string, @Body() data: Remark): Promise<Remark> {
-        return this.remarksService.update(id, data)
-}
-@Delete(':id')
+        return this.remarksService.update(id, data);
+    }
+
+    // Delete a remark
+    @Delete(':id')
     async delete(@Param('id') id: string): Promise<Remark> {
-        return this.remarksService.delete(id)
-}
+        return this.remarksService.delete(id);
+    }
 }
