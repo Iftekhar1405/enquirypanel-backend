c57456adac9fc857808304235947ec81f4f8d10c
Iftekhar Ahmed <152169376+Iftekhar1405@users.noreply.github.com>
2024-11-18 12:45:33 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,10 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
+// This export is necessary for Vercel serverless deployment
+export default async (req, res) => {
+  const app = await NestFactory.create(AppModule);
+  await app.init();
+  await app.listen(3000);
+  app.getHttpAdapter().getInstance().use(req, res); // Handles request and response
+};
