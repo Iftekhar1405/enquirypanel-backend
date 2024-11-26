bb7fc21d0a85e37d289b0c0b1de3521ba5809d0a
iftekhar1405 <iftekharahmedxyz@gmail.com>
2024-11-26 17:39:54 +0530

    sort functionality added

--- src/enquiry/enquiry.controller.ts
+++ src/enquiry/enquiry.controller.ts
@@ -1,45 +1,52 @@
-import {Body, Controller, Delete, Get, Param, Post, Put, Query, UseGuards} from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { EnquiryService } from './enquiry.service';
 import { Enquiry } from './schemas/enquiry.schema';
 
 @Controller('enquiry')
 export class EnquiryController {
-    constructor(private readonly enquiryService:EnquiryService){}
-    @Get()
-    async findAll():Promise<Enquiry[]>{
-        return this.enquiryService.findAll()
-    }
+  constructor(private readonly enquiryService: EnquiryService) {}
+  @Get()
+  async findAll(): Promise<Enquiry[]> {
+    return this.enquiryService.findAll();
+  }
 
-    @Get('search')
-    async search(
-        @Query('searchTerm') searchTerm: string,
-        @Query('filters') filters: string,
-        @Query('page') page:number,
-        @Query('limit') limit:number,
-    ) {
-        return this.enquiryService.search(searchTerm, filters,page,limit);
-    }
+  @Get('search')
+  async search(
+    @Query('searchTerm') searchTerm: string,
+    @Query('filters') filters: string,
+    @Query('page') page: number,
+    @Query('limit') limit: number,
+    @Query('sort') sort: string,
+  ) {
+    return this.enquiryService.search(searchTerm, filters, page, limit, sort);
+  }
 
-    @Get(':id')
-    async findOne(@Param('id') id:String):Promise<Enquiry>{
-        return this.enquiryService.findOne(id)
-    }
+  @Get(':id')
+  async findOne(@Param('id') id: String): Promise<Enquiry> {
+    return this.enquiryService.findOne(id);
+  }
 
+  @Put(':id')
+  async update(@Param('id') id: String, @Body() body: Partial<Enquiry>) {
+    return this.enquiryService.update(id, body);
+  }
 
+  @Post()
+  async createEnquiry(@Body() enquiryData: Enquiry): Promise<Enquiry> {
+    return this.enquiryService.create(enquiryData);
+  }
 
-    @Put(':id')
-    async update(@Param('id') id:String,@Body() body:Partial<Enquiry>){
-        return this.enquiryService.update(id,body)
-    }
-
-    @Post()
-    async createEnquiry(@Body() enquiryData:Enquiry):Promise<Enquiry>{
-        return this.enquiryService.create(enquiryData)
-    }
-
-    @Delete(':id')
-    async delete(@Param('id') id:String):Promise<String>{
-        return this.enquiryService.delete(id)
-    }
-
+  @Delete(':id')
+  async delete(@Param('id') id: String): Promise<String> {
+    return this.enquiryService.delete(id);
+  }
 }
--- src/enquiry/enquiry.service.ts
+++ src/enquiry/enquiry.service.ts
@@ -2,90 +2,108 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Enquiry, EnquiryDocument } from './schemas/enquiry.schema';
-import {query} from "express";
 
 @Injectable()
 export class EnquiryService {
-    constructor(@InjectModel(Enquiry.name) private readonly enquiryModel: Model<EnquiryDocument>) {
-    }
+  constructor(
+    @InjectModel(Enquiry.name)
+    private readonly enquiryModel: Model<EnquiryDocument>,
+  ) {}
 
-    async create(enquiryData: Partial<Enquiry>): Promise<Enquiry> {
-        const newEnquiry = new this.enquiryModel(enquiryData)
-        return newEnquiry.save()
-    }
+  async create(enquiryData: Partial<Enquiry>): Promise<Enquiry> {
+    const newEnquiry = new this.enquiryModel(enquiryData);
+    return newEnquiry.save();
+  }
 
-    async findAll(): Promise<Enquiry[]> {
-        return this.enquiryModel.find().exec()
-    }
+  async findAll(): Promise<Enquiry[]> {
+    return this.enquiryModel.find().exec();
+  }
 
-    async findOne(id: String): Promise<Enquiry> {
+  async findOne(id: String): Promise<Enquiry> {
+    return this.enquiryModel.findById(id).exec();
+  }
 
-        return this.enquiryModel.findById(id).exec()
-    }
+  async update(id: String, newData: Partial<Enquiry>): Promise<String> {
+    const updatedData = this.enquiryModel
+      .findByIdAndUpdate(id, newData, { new: true })
+      .exec();
+    if (updatedData)
+      return `data update successfully {Data_id:${id}} new data:${updatedData}`;
+    return `Could not update the data of id :${id} `;
+  }
 
-    async update(id: String, newData: Partial<Enquiry>): Promise<String> {
-        const updatedData = this.enquiryModel.findByIdAndUpdate(id, newData, {new: true}).exec()
-        if (updatedData) return `data update successfully {Data_id:${id}} new data:${updatedData}`
-        return `Could not update the data of id :${id} `
-    }
+  async delete(id: String): Promise<String> {
+    const deletedData = this.enquiryModel.findByIdAndDelete(id).exec();
+    if (deletedData)
+      return `data with id:${id} and data:${deletedData} deleted`;
+    return 'Could not delete';
+  }
 
-    async delete(id: String): Promise<String> {
-        const deletedData = this.enquiryModel.findByIdAndDelete(id).exec()
-        if (deletedData) return `data with id:${id} and data:${deletedData} deleted`
-        return 'Could not delete'
-    }
+  async search(
+    searchTerm: string,
+    filters: string,
+    page: number = 1,
+    limit: number = 20,
+    sort: string,
+  ) {
+    const filterQuery =
+      filters && filters !== 'undefined' ? JSON.parse(filters) : {};
 
-    async search(searchTerm: string, filters: string,page:number=1,limit:number=20) {
-        // Convert the filters string into an array of objects
-        const filterQuery = filters && filters !== 'undefined' ? JSON.parse(filters) : {};
+    const sortQuery = sort && sort !== 'undefined' ? JSON.parse(sort) : {};
+    Object.keys(sortQuery).forEach((key) => {
+      sortQuery[key] = sortQuery[key] === 'asc' ? 1 : -1;
+    });
 
-        // Prepare the search query, if there's a searchTerm, search across multiple fields
-        const searchQuery = searchTerm
-            ? {
-                $or: [
-                    { description: { $regex: searchTerm, $options: 'i' } },
-                    { studentFirstName: { $regex: searchTerm, $options: 'i' } },
-                    { studentLastName: { $regex: searchTerm, $options: 'i' } },
-                    { grade: { $regex: searchTerm, $options: 'i' } },
-                    { guardianName: { $regex: searchTerm, $options: 'i' } },
-                    { email: { $regex: searchTerm, $options: 'i' } },
-                    { phone: { $regex: searchTerm, $options: 'i' } },
-                    { mobile: { $regex: searchTerm, $options: 'i' } },
-                    { city: { $regex: searchTerm, $options: 'i' } },
-                    { state: { $regex: searchTerm, $options: 'i' } },
-                    { zip: { $regex: searchTerm, $options: 'i' } },
-                    { country: { $regex: searchTerm, $options: 'i' } },
-                    // { hostel: { $regex: searchTerm, $options: 'i' } },
-                    { gender: { $regex: searchTerm, $options: 'i' } },
-                    { enquirySource: { $regex: searchTerm, $options: 'i' } },
-                    { street: { $regex: searchTerm, $options: 'i' } },
-                    { currentSchool: { $regex: searchTerm, $options: 'i' } },
-                    // { dob: searchTerm },
-                    { twitter: { $regex: searchTerm, $options: 'i' } },
-                ],
-            }
-            : {};
-            console.log(filterQuery)
-        Object.keys(filterQuery).forEach((item : any)=>{
-            let value = filterQuery[item]
-            console.log(item)
-            if(item != "hostel" && item != 'gender'){//hj
-                filterQuery[item] = {$regex:value,$options:'i'}}
+    const searchQuery = searchTerm
+      ? {
+          $or: [
+            { description: { $regex: searchTerm, $options: 'i' } },
+            { studentFirstName: { $regex: searchTerm, $options: 'i' } },
+            { studentLastName: { $regex: searchTerm, $options: 'i' } },
+            { grade: { $regex: searchTerm, $options: 'i' } },
+            { guardianName: { $regex: searchTerm, $options: 'i' } },
+            { email: { $regex: searchTerm, $options: 'i' } },
+            { phone: { $regex: searchTerm, $options: 'i' } },
+            { mobile: { $regex: searchTerm, $options: 'i' } },
+            { city: { $regex: searchTerm, $options: 'i' } },
+            { state: { $regex: searchTerm, $options: 'i' } },
+            { zip: { $regex: searchTerm, $options: 'i' } },
+            { country: { $regex: searchTerm, $options: 'i' } },
+            // { hostel: { $regex: searchTerm, $options: 'i' } },
+            { gender: { $regex: searchTerm, $options: 'i' } },
+            { enquirySource: { $regex: searchTerm, $options: 'i' } },
+            { street: { $regex: searchTerm, $options: 'i' } },
+            { currentSchool: { $regex: searchTerm, $options: 'i' } },
+            // { dob: searchTerm },
+            { twitter: { $regex: searchTerm, $options: 'i' } },
+          ],
+        }
+      : {};
+    console.log(filterQuery);
+    Object.keys(filterQuery).forEach((item: any) => {
+      let value = filterQuery[item];
+      console.log(item);
+      if (item != 'hostel' && item != 'gender') {
+        filterQuery[item] = { $regex: value, $options: 'i' };
+      }
+    });
+    console.log(filterQuery);
+    const finalQuery = {
+      ...searchQuery,
+      ...filterQuery,
+    };
+    console.log(sortQuery, sort);
+    const skip = (page - 1) * limit;
+    const data = await this.enquiryModel
+      .find(finalQuery)
+      .sort(sortQuery)
+      .collation({ locale: 'en', strength: 5 })
+      .skip(skip)
+      .limit(limit)
+      .exec();
+    const count = await this.enquiryModel.countDocuments().exec();
+    const pageCount = Math.ceil(count / limit);
 
-
-        })
-        console.log(filterQuery)
-        const finalQuery = {
-            ...searchQuery,
-            ...filterQuery, 
-        };
-
-        const skip = (page - 1)*limit
-        const data = await this.enquiryModel.find(finalQuery).skip(skip).limit(limit).exec()
-        const count = await this.enquiryModel.countDocuments().exec()
-        const pageCount = Math.ceil(count/limit) 
-        
-        return {data,count,pageCount} ;
-    }
-
+    return { data, count, pageCount };
+  }
 }
